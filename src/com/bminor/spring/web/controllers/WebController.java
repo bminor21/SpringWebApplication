ab51b432b824e0b8ded7fb3ad34a79c7ff09d099
Brett Minor <brett.minor21@gmail.com>
2017-01-09 21:21:19 -0500

    Added JSTL jar and header.

--- src/com/bminor/spring/web/controllers/WebController.java
+++ src/com/bminor/spring/web/controllers/WebController.java
@@ -1,13 +1,15 @@
 package com.bminor.spring.web.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class WebController {
 	
 	@RequestMapping("/")
-	public String showHome() {
+	public String showHome( Model model ) {
+		model.addAttribute( "name", "Brett" );
 		return "Home";
 	}
 	
